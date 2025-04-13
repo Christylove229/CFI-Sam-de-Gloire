24fed4364c6de27251b8dce6d6e4ed0220d34f3c
Christylove229 <christylovedovonon@gmail.com>
2025-04-13 14:04:49 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/CFI-Sam-de-Gloire/",
   server: {
     host: "::",
     port: 8080,
